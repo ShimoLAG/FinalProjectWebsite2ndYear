364789ff5604f409fb216880c3e3538c1b5eb15f
ShimoLAG <161853425+ShimoLAG@users.noreply.github.com>
2024-05-13 22:02:04 +0800

    Add files via upload

---
+++ FinalProjectWebsiteTest-main/Website base/website/views.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+#importing Blueprint just means this .py has a bunch or urls defined in here
+
+
+views = Blueprint('views', __name__) #defining a blueprint
+
+@views.route('/')  #this is how we make a route to and when we go to this url it runs the function below it
+def landingpage():
+    return render_template("landingpage.html")
+
+
+@views.route('/home')
+def home():
+    return render_template("home.html")
