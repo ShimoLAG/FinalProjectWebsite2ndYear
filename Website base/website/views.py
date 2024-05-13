56aea071de1b046580f6e266a65c385094500895
ShimoLAG <161853425+ShimoLAG@users.noreply.github.com>
2024-05-13 22:07:51 +0800

    Add files via upload

--- Website base/website/views.py
+++ Website base/website/views.py
@@ -8,3 +8,8 @@ views = Blueprint('views', __name__) #defining a blueprint
 @views.route('/')  #this is how we make a route to and when we go to this url it runs the function below it
 def landingpage():
     return render_template("landingpage.html")
+
+
+@views.route('/home')
+def home():
+    return render_template("home.html")
