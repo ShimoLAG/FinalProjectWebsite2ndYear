13d0dd3dc44ab0e98f762629479918a3c6721d3f
ShimoLAG <161853425+ShimoLAG@users.noreply.github.com>
2024-05-20 01:38:21 +0800

    Add files via upload

--- Website base/website/views.py
+++ Website base/website/views.py
@@ -79,12 +79,17 @@ def itinerary(travelID):
         else:
             return None
     
-    def get_itineraries(travelID):
+    def get_itineraries_and_travel_name(travelID):
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT itineraryID, itineraryDay, itineraryName, itineraryDescription, travelID FROM itineraries WHERE travelID = %s', (travelID,))
-        itineraries = cursor.fetchall()
+        cursor.execute('''
+            SELECT i.itineraryID, i.itineraryDay, i.itineraryName, i.itineraryDescription, i.travelID, t.travelName 
+            FROM itineraries i
+            JOIN travel t ON i.travelID = t.travelID
+            WHERE i.travelID = %s
+        ''', (travelID,))
+        data = cursor.fetchall()
         cursor.close()
-        return itineraries
+        return data
 
     if request.method == 'POST':
         itineraryDay = request.form['itineraryDay']
@@ -105,17 +110,107 @@ def itinerary(travelID):
         flash("Not logged in", category='error')
         return redirect(url_for('auth.login'))  # Redirect to login page
     
-    itineraries = get_itineraries(travelID)
+    itineraries_and_travel = get_itineraries_and_travel_name(travelID)
     
-    return render_template('itinerary.html', itineraries=itineraries, travelID=travelID)
+    return render_template('itinerary.html', itineraries=itineraries_and_travel, travelID=travelID)
 
 
-@views.route('/destinations')
+
+@views.route('/destinations/<int:itineraryID>', methods=['GET', 'POST'])
 @login_required
-def destinations():
-    return render_template("destinations.html")
+def destinations(itineraryID):  # Ensure itineraryID is passed as a parameter
+    def get_account_id():
+        if current_user.is_authenticated:
+            return current_user.accountID
+        else:
+            return None
+    
+    def get_destinations(itineraryID):
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT destinationID, zipcode, destinationName, cityName, regionName, itineraryID FROM destinations WHERE itineraryID = %s', (itineraryID,))
+        destinations = cursor.fetchall()
+        cursor.close()
+        return destinations
+    
+    if request.method == 'POST':
+        zipcode = request.form['zipcode']
+        destinationName = request.form['destinationName']
+        cityName = request.form['cityName']
+        regionName = request.form['regionName']
+        
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO destinations (zipcode, destinationName, cityName, regionName, itineraryID) VALUES (%s, %s, %s, %s, %s)", (zipcode, destinationName, cityName, regionName, itineraryID))
+        mysql.connection.commit()
+        cursor.close()
+        
+        # Redirect to the same page to avoid resubmission on page refresh
+        return redirect(url_for('views.destinations', itineraryID=itineraryID))  # Corrected url_for destination
 
-@views.route('/activities')
+    # Fetch destination data for the given itineraryID and render the template
+    accountID = get_account_id()
+    if accountID is None:
+        flash("Not logged in", category='error')
+        return redirect(url_for('auth.login'))  # Redirect to login page
+    
+    destinations = get_destinations(itineraryID)
+    
+    return render_template("destinations.html", destinations=destinations, itineraryID=itineraryID)
+
+
+@views.route('/activities/<int:destinationID>', methods=['GET', 'POST'])
 @login_required
-def activities():
-    return render_template("activities.html")
+def activities(destinationID):
+    """
+    Define a Flask view to handle GET and POST requests for activities associated with a specific destination identified by destinationID.
+    """
+    def get_account_id():
+        """
+        Helper function to retrieve the account ID of the current logged-in user.
+        """
+        if current_user.is_authenticated:
+            return current_user.accountID
+        else:
+            return None
+    
+    def get_activities(destinationID):
+        """
+        Function to fetch activities associated with the given destinationID from the database.
+        """
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT activityID, activityName, activityDescription, timeFrom, timeTo, address, activitySequence, destinationID FROM activities WHERE destinationID = %s', (destinationID,))
+        activities = cursor.fetchall()
+        cursor.close()
+        return activities
+    
+    if request.method == 'POST':
+        """
+        Handling POST requests to add new activities associated with the destination.
+        """
+        activityName = request.form['activityName']
+        timeFrom = request.form['timeFrom']
+        timeTo = request.form['timeTo']
+        address = request.form['address']
+        activityDescription= request.form['activityDescription']
+
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT IFNULL(MAX(activitySequence), 0) + 1 FROM activities WHERE destinationID = %s", (destinationID,))
+        activitySequence = cursor.fetchone()[0]
+
+        
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO activities (activityName, timeFrom, timeTo, address, activityDescription, destinationID, activitySequence) VALUES (%s, %s, %s, %s, %s, %s, %s)", (activityName, timeFrom, timeTo, address, activityDescription, destinationID, activitySequence))
+        mysql.connection.commit()
+        cursor.close()
+        
+        # Redirect to the same page to avoid resubmission on page refresh
+        return redirect(url_for('views.activities', destinationID=destinationID))
+    
+    # Fetch activity data for the given destinationID and render the template
+    accountID = get_account_id()
+    if accountID is None:
+        flash("Not logged in", category='error')
+        return redirect(url_for('auth.login'))  # Redirect to login page
+    
+    activities = get_activities(destinationID)
+    
+    return render_template("activities.html", activities=activities, destinationID=destinationID)
